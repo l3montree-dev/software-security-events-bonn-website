a40cdb53a63a225b4ecbb6b4f4acdd7b0adc82b9
Frederic Noppe <frederic.noppe@l3montree.com>
2025-03-13 19:42:49 +0100

    updates the website
    Signed-off-by: Frederic Noppe <frederic.noppe@l3montree.com>

--- src/components/oss-bonn/Topic.tsx
+++ src/components/oss-bonn/Topic.tsx
@@ -62,7 +62,7 @@ export default function Topic() {
             </div>
           </div>
           <img
-            src="../oss-event.png"
+            src="/OSS-Event.png"
             className="w-[48rem] max-w-none rounded-lg shadow-xl shadow-black/10 ring-1 ring-white/10 sm:w-[57rem] md:-ml-4 lg:-ml-0 lg:block"
             width={1216}
             height={721}
