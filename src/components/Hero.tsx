c0a93931aa4079d5b05ba609f4361218fa9cbf00
Frederic Noppe <frederic.noppe@l3montree.com>
2025-09-03 17:04:30 +0200

    Changes Texts in Hero and Topic
    Signed-off-by: Frederic Noppe <frederic.noppe@l3montree.com>

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -48,7 +48,7 @@ export function Hero() {
                   </strong>
                 </li>
                 <li>
-                  <strong> Wie wird die Cloud des Bundes souverän?</strong>
+                  <strong> Wie wird und bleibt die Cloud des Bundes souverän?</strong>
                 </li>
                 <li>
                   <strong>
